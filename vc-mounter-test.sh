95c2c5887bfd5b14448eab4252a3b51bbe723781
Alberto Cavalin <acavalin@users.noreply.github.com>
2018-03-01 08:37:42 +0100

    * vc-mounter.service - add reboot.target halt.target kexec.target support + some extra paranoia * vc-mounter-test.sh - append logs + show all mounts * tmpfs-folders.sh   - update howto comment for /var/log setup

--- vc-mounter-test.sh
+++ vc-mounter-test.sh
@@ -2,29 +2,38 @@
 
 log=/opt/systemd-units/vc-mounter.log
 
-echo "--- `date` ----------------------------------" > $log
+echo "--- `date` ----------------------------------" >> $log
+
 echo '---------- mount + df' >> $log
 echo >> $log; mount | grep loop >> $log
 echo >> $log; df -h | grep loop >> $log
+echo '---------- mount all' >> $log
+mount >> $log
 echo '---------- veracrypt -l' >> $log
-/opt/bin/veracrypt -l >> $log
+/opt/bin/veracrypt -t -l >> $log
 echo '---------- losetup' >> $log
 losetup -l >> $log
 
 echo '---------- try-umount' >> $log
 /opt/systemd-units/vc-mounter.rb try-umount >> $log 2>&1
 
+echo '----------------------------------------------' >> $log
+
 echo '---------- mount + df' >> $log
 echo >> $log; mount | grep loop >> $log
 echo >> $log; df -h | grep loop >> $log
+echo '---------- mount all' >> $log
+mount >> $log
 echo '---------- veracrypt -l' >> $log
-/opt/bin/veracrypt -l >> $log
+/opt/bin/veracrypt -t -l >> $log
 echo '---------- losetup' >> $log
 losetup -l >> $log
 
+echo '----------------------------------------------' >> $log
+
 echo '---------- ls /dev*' >> $log
 ls -l /dev/shm/vc-mounter/ >> $log
-ls -l /dev/sda* >> $log
+ls -l /dev/sd[ab]* >> $log
 
 echo '---------- ps ax' >> $log
 ps ax >> $log
